294ab745d6df7742f2b6c8e78b76517bc7bc8498
Bokhtiar Rifat <mdbrifat@gmail.com>
2023-01-19 00:09:32 +0600

    Modified Code

--- W3Resource/Functions/Program.cs
+++ W3Resource/Functions/Program.cs
@@ -1,4 +1,4 @@
-﻿//                                                  001
+﻿// 001
 //Write a program in C# Sharp to create a recursive function to calculate the Fibonacci number of a specific term
 //---------------------------------------------------------------------------------------------------------------
 
@@ -27,7 +27,7 @@
 //int value = Factorial(9);
 //Console.WriteLine(value);
 
-//int Factorial(int x)                      // ITERATIVE METHOD         
+//int Factorial(int x)          // ITERATIVE METHOD         
 //{
 //    int sum = 1;
 
@@ -41,7 +41,7 @@
 //}
 
 
-//int Factorial(int x)                     // RECURSIVE METHOD                              
+//int Factorial(int x)      // RECURSIVE METHOD                              
 //{
 //    if (x == 0)
 //    {
@@ -54,3 +54,52 @@
 //}
 
 //----------------------------------------------------------------------------------------------------------------
+
+//003
+
+//Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number. Go to the editor
+//Test Data :
+//Enter a number: 1234
+//Expected Output :
+//The sum of the digits of the number 1234 is : 10
+
+//Console.WriteLine("Enter a number");
+//int result = SumNumber(int.Parse(Console.ReadLine()));
+//Console.WriteLine($"the sum of the individual digit is {result}");
+//int remainder;
+//int SumNumber(int x)
+//{
+//    int sum = 0;
+//    while (x > 0)
+//    {
+//        remainder = x % 10;
+//        sum += remainder;
+//        x = x / 10;
+//    }
+//    return sum;
+//}
+
+// OR
+//Console.WriteLine("Enter a number");
+//var result = SumNumber(int.Parse(Console.ReadLine()));
+//Console.WriteLine(result);
+//int SumNumber(int n)
+//{
+//    string x = n.ToString();
+//    var sum = 0;
+//    for (int i = 0; i < x.Length; i++)
+//    {
+//        sum = sum + Convert.ToInt32(x.Substring(i,1));
+//    }
+//    return sum;
+//   }
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
